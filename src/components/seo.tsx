8e05688420c769da20a0befaa6fd2eff5d3c3e25
Samuele Musazzi <samuele.musazzi@garaio.com>
2022-03-04 12:11:25 +0100

    initial commit

---
+++ src/components/seo.tsx
@@ -0,0 +1,81 @@
+import { Helmet } from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
+
+interface Meta {
+  content: any
+  property: string
+  name?: undefined
+}
+
+interface SeoProps {
+  description?: string
+  lang?: string
+  title: string
+
+  meta?: any
+}
+
+function Seo({ description = "", lang = "en", meta = [], title }: SeoProps) {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+          }
+        }
+      }
+    `
+  )
+
+  const metaDescription = description || site.siteMetadata.description
+  const defaultTitle = site.siteMetadata?.title
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title={title}
+      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : undefined}
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:title`,
+          content: title,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary`,
+        },
+        {
+          name: `twitter:creator`,
+          content: site.siteMetadata?.author || ``,
+        },
+        {
+          name: `twitter:title`,
+          content: title,
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription,
+        },
+      ].concat(meta)}
+    />
+  )
+}
+
+export default Seo
